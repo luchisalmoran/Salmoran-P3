a0f4fa74ed372f9a2f9979cceed307cc269dd11a
luz <122lsalmoran@frhsd.com>
2022-03-22 09:47:22 -0400

    work pt1-2 almost lol

--- site.js
+++ site.js
@@ -32,9 +32,9 @@ const vue_app = Vue.createApp({
       data() {
         return {
             // This holds your movies.json data.
-            movies: [],
+            movies: [ ],
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-      }
+            github: "https://luchisalmoran.github.io/Salmoran-P3/"
     },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
